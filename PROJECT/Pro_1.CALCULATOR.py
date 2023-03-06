b7e7ef62fe9381ce35e3543cc69c78c21ce5fe04
Sayli Ganpat <112109540+CodeExplorer2104@users.noreply.github.com>
2023-03-06 23:23:48 +0530

    Add files via upload

---
+++ PROJECT/Pro_1.CALCULATOR.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sayli
+#
+# Created:     13-08-2022
+# Copyright:   (c) sayli 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+first=input("ENTER 1st digit:")
+second=input("ENTER 2nd digit :")
+first=int(first)
+second=int(second)
+operator=input("ENTER OPERATOR:")
+if operator=="+":
+    print(first + second)
+elif operator=="/":
+    print(first / second)
+elif operator =="%":
+    print(first%second)
+elif operator=="*":
+    print(first * second)
+else :
+    print("invalid operation ")
